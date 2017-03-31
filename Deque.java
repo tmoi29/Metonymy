0e20a43181193cd7634289c8f9a610b02ab15245
Ryan Siu <rsiu@stuy.edu>
2017-03-31 11:34:52 -0400

    added interface and updated gitignore

---
+++ Deque.java
@@ -0,0 +1,17 @@
+public interface Deque<T> {
+
+    public void addFirst( T foo );
+    
+    public void addLast( T foo );
+
+    public T removeFirst();
+
+    public T removeLast();
+
+    public T getFirst();
+
+    public T getLast();
+
+    public int size();
+    
+}
