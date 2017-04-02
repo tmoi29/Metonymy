739e5b0482b9299db42e119b19834f9c063de0b7
Ryan Siu <rsiu@stuy.edu>
2017-04-02 17:38:44 -0400

    added some methods

--- DLLNode.java
+++ DLLNode.java
@@ -1,18 +1,19 @@
+/*****************************************************
+ * class DLLNode
+ * Implements a node, for use in lists and other container classes.
+ *****************************************************/
+
 public class DLLNode<T> 
 {
+    private T _cargo;    //cargo may only be of type T
+    private DLLNode<T> _nextNode, _prevNode; //pointers to next, prev DLLNodes
 
-    private T _cargo;    //cargo may only be of type String
-    private DLLNode<T> _nextNode; //pointer to next LLNode
-    private DLLNode<T> _previousNode; //pointer to last LLNode
 
     // constructor -- initializes instance vars
-    public DLLNode( T value, DLLNode<T> next ) 
-    {
+    public DLLNode( T value, DLLNode<T> prev, DLLNode<T> next ) {
 	_cargo = value;
 	_nextNode = next;
-	if (_cargo != null && _nextNode != null){
-	    _nextNode.setPrevious(this);
-	}
+	_prevNode = prev;
     }
 
 
@@ -20,31 +21,28 @@ public class DLLNode<T>
     public T getCargo() { return _cargo; }
 
     public DLLNode<T> getNext() { return _nextNode; }
-    
-    public DLLNode<T> getPrevious() {return _previousNode;}
+
+    public DLLNode<T> getPrev() { return _prevNode; }
     //--------------^  ACCESSORS  ^--------------
 
 
     //--------------v  MUTATORS  v--------------
-    public T setCargo( T newCargo ) 
-    {
-	T old = _cargo;
+    public T setCargo( T newCargo ) {
+	T foo = getCargo();
 	_cargo = newCargo;
-	return old;
+	return foo;
     }
 
-    public DLLNode<T> setNext( DLLNode<T> newNext ) 
-    {
-	DLLNode<T> old = _nextNode;
+    public DLLNode<T> setNext( DLLNode<T> newNext ) {
+	DLLNode<T> foo = getNext();
 	_nextNode = newNext;
-	return old;
+	return foo;
     }
-    
-    public DLLNode<T> setPrevious( DLLNode<T> newPrev ) 
-    {
-	DLLNode<T> old = _previousNode;
-	_previousNode = newPrev;
-	return old;
+
+    public DLLNode<T> setPrev( DLLNode<T> newPrev ) {
+	DLLNode<T> foo = getPrev();
+	_prevNode = newPrev;
+	return foo;
     }
     //--------------^  MUTATORS  ^--------------
 
@@ -52,35 +50,4 @@ public class DLLNode<T>
     // override inherited toString
     public String toString() { return _cargo.toString(); }
 
-
-    //main method for testing
-    public static void main( String[] args ) 
-    {
-
-
-	//Below is an exercise in creating a linked list...
-
-	//Create a node
-	DLLNode<String> first = new DLLNode<String>( "cat", null );
-
-	//Create a new node after the first
-	first.setNext( new DLLNode<String>( "dog", null ) );
-
-	//Create a third node after the second
-	first.getNext().setNext( new DLLNode<String>( "cow", null ) );
-
-	/* A naive list traversal:*/
-	while( first != null ) {
-	    System.out.println( first );
-	    first = first.getNext();
-	}
-
-	//Q: When head ptr moves to next node in list,
-	//   what happens to the node it just left?
-	// It forgets about the node that it just left and it continues
-	// to the next node.
-	
-
-    }//end main
-
-}//end class LLNode
+}//end class DLLNode
--- Driver.java
+++ Driver.java
@@ -1,31 +1,124 @@
-public class Driver<T> implements Deque<T>{
+import java.util.NoSuchElementException;
+
+public class Driver<T> implements Deque<T> {
+    
     private int _size;
     private DLLNode<T> _front;
     private DLLNode<T> _end;
     
-    public driver(){
+    public Driver() {
 	_front = null;
 	_end = null;
 	_size = 0;
     }
+    
     public void addFirst( T foo ){
-	DLLNode<T> add = new DLLNode(foo, _front);
-	    _front = add;
+	DLLNode<T> add = new DLLNode( foo, null, null );
+	if ( isEmpty() ) {
+	    _end = add;
+	} else {
+	    add.setNext( _front );
+	    _front.setPrev( add );
+	}
+	_front = add;
+	_size++;
     }
     
-    public void addLast( T foo );
+    public void addLast( T foo ) {
+	DLLNode<T> add = new DLLNode( foo, null, null );
+	if ( isEmpty() ) {
+	    _front = add;
+	} else {
+	    add.setPrev( _end );
+	    _end.setNext( add );
+	}
+	_end = add;
+	_size++;
+    }
 
-    public T removeFirst();
+    public T removeFirst() {
+	if ( isEmpty() ) {
+	    throw new NoSuchElementException( "Deque is empty, element cannot be removed" );
+	}
+	
+	T foo = _front.getCargo();
+	
+	if ( size() == 1 ) {
+	    _front = null;
+	    _end = null;
+	} else {
+	    _front = _front.getNext();
+	    _front.setPrev( null );
+	}
 
-    public T removeLast();
+	_size--;
+	return foo;
+    }
 
-    public T getFirst();
+    public T removeLast() {
+	if ( isEmpty() ) {
+	    throw new NoSuchElementException( "Deque is empty, element cannot be removed" );
+	}
+	
+	T foo = _end.getCargo();
 
-    public T getLast();
+	if ( size() == 1 ) {
+	    _front = null;
+	    _end = null;
+	} else {
+	    _end = _end.getPrev();
+	    _end.setNext( null );
+	}
 
-    public int size();
+	_size--;
+	return foo;
+    }
+    
+    public T getFirst() {
+	return null;
+    }
+
+    public T getLast() {
+	return null;
+    }
+
+    public int size() {
+	return _size;
+    }
 
     public boolean isEmpty(){
 	return _size == 0;
+    }
+
+    public String toString() {
+	DLLNode<T> foo = _front;
+	String retStr = "";
+	while ( foo != null ) {
+	    retStr += foo.getCargo() + " ";
+	    foo = foo.getNext();
+	}
+	return retStr;
+    }
+
+    public static void main( String[] args ) {
+	Driver d = new Driver();
+	d.addLast( "e" );
+	d.addFirst( "hi" );
+	d.addFirst( "Hello" );
+	d.addFirst( "Bye" );
+	d.addLast( "a" );
+	d.addLast( "b" );
+	d.addLast( "c" );
+	System.out.println( d );
+	d.removeFirst();
+	d.removeLast();
+	d.removeFirst();
+	d.removeLast();
+	d.removeLast();
+	d.removeFirst();
+	System.out.println( d );
+	d.removeFirst();
+	System.out.println( d );
+	d.removeFirst(); // error
     }
 }
