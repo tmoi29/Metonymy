2a0b022c360b0c30fb85ffe59ef3b42383ea2c1c
Azalea126 <tmoi@stuy.edu>
2017-04-03 11:39:16 -0400

    added comments

--- Driver.java
+++ Driver.java
@@ -1,22 +1,43 @@
 import java.util.NoSuchElementException;
 
+/*****************************************************
+ * class DLLQueue
+ * uses a doubly-linked nodes to implement a QUEUE
+ * (a collection with FIFO property)
+ *
+ *         -------------------------------
+ *   front |  <-->   Q U E U E   <-->    | end
+ *         -------------------------------
+ *
+ ******************************************************/
+
 public class Driver<T> implements Deque<T> {
-    
+    //declaring instance variables
     private int _size;
     private DLLNode<T> _front;
     private DLLNode<T> _end;
     
+    /*=============================
+      Driver() -- default constuctor
+      Initialing instance variables
+      =============================*/
     public Driver() {
 	_front = null;
 	_end = null;
 	_size = 0;
     }
-    
+    /*=============================
+      addFirst() -- adds the element to the front of the DLList
+      precond: an object is given
+      postcond: adds element to the front of the list
+      =============================*/
     public void addFirst( T foo ){
 	DLLNode<T> add = new DLLNode( foo, null, null );
+	//is it the first element??
 	if ( isEmpty() ) {
 	    _end = add;
-	} else {
+	} 
+	else {
 	    add.setNext( _front );
 	    _front.setPrev( add );
 	}
@@ -24,8 +45,14 @@ public class Driver<T> implements Deque<T> {
 	_size++;
     }
     
+    /*=============================
+      addLast() -- adds the element to the end of the DLList
+      precond: an object is given
+      postcond: adds element to the end of the list
+      =============================*/
     public void addLast( T foo ) {
 	DLLNode<T> add = new DLLNode( foo, null, null );
+	//is it the first element??
 	if ( isEmpty() ) {
 	    _front = add;
 	} else {
@@ -35,18 +62,25 @@ public class Driver<T> implements Deque<T> {
 	_end = add;
 	_size++;
     }
-
+    /*=============================
+      removeFirst() -- removes the element at the front of the DLList
+      postcond: removes the first element and reassigns the pointers
+      =============================*/
     public T removeFirst() {
+	//checks if the list is empty
 	if ( isEmpty() ) {
 	    throw new NoSuchElementException( "Deque is empty, element cannot be removed" );
 	}
-	
+	//removing value
 	T foo = _front.getCargo();
 	
+	//removing the last value in the list
 	if ( size() == 1 ) {
 	    _front = null;
 	    _end = null;
-	} else {
+	} 
+	//removing the first value and replacing the pointer to it with null
+	else {
 	    _front = _front.getNext();
 	    _front.setPrev( null );
 	}
@@ -54,18 +88,26 @@ public class Driver<T> implements Deque<T> {
 	_size--;
 	return foo;
     }
-
+    /*=============================
+      removeLast() -- removes the element at the end of the list
+      postcond: removes the element at the end of the list and reassigns the end pointer
+      =============================*/
     public T removeLast() {
+	//checks if the list is already empty
 	if ( isEmpty() ) {
 	    throw new NoSuchElementException( "Deque is empty, element cannot be removed" );
 	}
 	
+	//removing value
 	T foo = _end.getCargo();
 
+	//removing the last value
 	if ( size() == 1 ) {
 	    _front = null;
 	    _end = null;
-	} else {
+	}
+	//removes value and reassigns the last pointer
+	else {
 	    _end = _end.getPrev();
 	    _end.setNext( null );
 	}
