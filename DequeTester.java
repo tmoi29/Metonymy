d21d3dba1562a896a02f2473136ea3b65b817812
Ryan Siu <rsiu@stuy.edu>
2017-04-04 15:28:50 -0400

    added comments

--- DequeTester.java
+++ DequeTester.java
@@ -13,28 +13,33 @@ public class DequeTester {
 
 	// Test using deque of Strings
 	System.out.println( "\nTesting deque of Strings" );
-	DLLDeque<String> stringDeque = new DLLDeque<String>();
+	DLLDeque<String> stringDeque = new DLLDeque<String>(); //error if generic typing is not allowed
 	System.out.println( stringDeque );
 	System.out.println( "\nAdding elements..." );
+	// a, b, c should be added to the deque
 	stringDeque.addLast( "b" );
 	stringDeque.addFirst( "a" );
 	stringDeque.addLast( "c" );
 	System.out.println( stringDeque ); // a b c
 	System.out.println( "\nRemoving elements..." );
+	// a and c should be removed from the deque
 	stringDeque.removeFirst();
 	stringDeque.removeLast();
 	System.out.println( stringDeque ); // b
-	System.out.println( "\nRemoving last element..." );
+	System.out.println( "\nRemoving remaining element..." );
+	// b should be removed from the deque
 	stringDeque.removeFirst();
 	System.out.println( stringDeque ); // empty deque
 
 	System.out.println( "\nRemvoing element..." );
+	// should print exception if exceptions are handled
 	try {
 	    stringDeque.removeFirst(); // error
 	} catch (NoSuchElementException e) {
 	    e.printStackTrace(); //prints error message
 	}
 	System.out.println( "\nGetting first element..." );
+	// should print exception if exceptions are handled
 	try {
 	    stringDeque.getFirst(); //error
 	} catch (NoSuchElementException e) {
@@ -42,8 +47,10 @@ public class DequeTester {
 	}
 
 	System.out.println( "\nAdding element..." );
+	// a should be added to the deque
 	stringDeque.addFirst( "a" );
 	System.out.println( "\nGetting first element..." );
+	// a should be printed
 	System.out.println( stringDeque.getFirst() + "\n" ); // a
 
 
@@ -51,14 +58,18 @@ public class DequeTester {
 	System.out.println( "\nTesting deque of Objects" );
 	DLLDeque mixDeque = new DLLDeque();
 	System.out.println( "\nAdding elements..." );
+	// elements should be added to the deque
 	mixDeque.addFirst( 1 );
 	mixDeque.addLast( "a" );
 	mixDeque.addFirst( new ArrayList<Integer>() );
+	// elements should be properly printed in order
 	System.out.println( mixDeque );
 	System.out.println( "\nAdding 64 to the ArrayList at front" );
+	// first element should be removed
 	ArrayList a = (ArrayList)mixDeque.removeFirst(); //pop the ArrayList
 	a.add( 64 ); //add element 64
-	mixDeque.addFirst( a ); // put ArrayList back in with 64 element
+	// ArrayList containing 64 should be added to the deque
+	mixDeque.addFirst( a );
 	System.out.println( mixDeque ); // [64] 1 a
 	
     }
